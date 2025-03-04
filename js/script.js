c7634e92a881d8d1d72a570372df805932e3267a
gchimar <gonzalochitomarquez@gmail.com>
2025-03-04 17:48:01 +0100

    subir proyecto

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  const showMessageButton = document.getElementById('showMessageButton');
+  showMessageButton.addEventListener('click', () => {
+    alert('Como mola la astronomia');
+  });
+
+  const showImgButton = document.getElementById('showImgButton');
+  showImgButton.addEventListener('click', () => {
+    window.open("img/khat.gif", "_blank", "width=600,height=400");
+  });
+});
